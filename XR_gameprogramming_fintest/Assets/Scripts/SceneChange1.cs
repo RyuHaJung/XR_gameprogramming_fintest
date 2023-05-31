8bcceabc1371668053ac89f66c43c47d7a17f0cf
RyuHaJung <red061011@gmail.com>
2023-05-31 17:43:48 +0900

    씬 이동 및 수정

---
+++ XR_gameprogramming_fintest/Assets/Scripts/SceneChange1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange1 : MonoBehaviour
+{
+    public void ChangeNextScene()
+    {
+
+        SceneManager.LoadScene(1);
+
+    }
+
+}
